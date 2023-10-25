430dd4e5a97ac4a392a6a6e29b6fca82f2bac88f
Toyyib <hamzatbolaji648@yahoo.com>
2023-10-25 17:25:14 +0100

    process data in model

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,4 +2,13 @@ class Like < ApplicationRecord
   # Associations
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+    # Methods
+    after_save :update_post_likes_counter
+
+    private
+
+    def update_post_likes_counter
+      post.increment!(:likes_counter)
+    end
 end
